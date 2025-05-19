e64bb1926192093684de86e49cd6517e17ebd518
FranJava <2006fran01@gmail.com>
2025-05-18 23:25:05 -0600

    cv completo sin poder descargarlo a pdf

--- OpusFinder/cv.cs
+++ OpusFinder/cv.cs
@@ -4,9 +4,14 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
 
 namespace OpusFinder
 {
@@ -24,5 +29,78 @@ namespace OpusFinder
             form1.Show();
             this.Close();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cv_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog archivo = new OpenFileDialog();
+            archivo.Filter = "Archivos de imagen (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png|Todos los archivos (*.*)|*.*";
+            archivo.Title = "Seleccionar imagen de perfil";
+            if (archivo.ShowDialog() == DialogResult.OK)
+            {
+                // Cargar la imagen seleccionada en el PictureBox
+                pictureBoxFoto.Image = System.Drawing.Image.FromFile(archivo.FileName);
+                pictureBoxFoto.SizeMode = PictureBoxSizeMode.StretchImage; // Ajustar el tamaño de la imagen al PictureBox
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            
+        }
     }
+    
 }
