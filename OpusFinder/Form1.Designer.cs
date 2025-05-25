c40f5b28abeccdc1b956a4f3d0e810a2de3da713
FranJava <2006fran01@gmail.com>
2025-05-25 16:23:24 -0600

    arreglando

--- OpusFinder/Form1.Designer.cs
+++ OpusFinder/Form1.Designer.cs
@@ -799,7 +799,7 @@
             pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox4.TabIndex = 28;
             pictureBox4.TabStop = false;
-            pictureBox4.Click += pictureBox4_Click;
+            
             // 
             // descripcion
             // 
