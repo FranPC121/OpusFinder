24d0b08e2bc2c15a0ed6f265bea09b5e44daebd2
FranJava <2006fran01@gmail.com>
2025-05-25 20:40:20 -0600

    ff

--- OpusFinder/cv.cs
+++ OpusFinder/cv.cs
@@ -55,11 +55,8 @@ namespace OpusFinder
             this.Hide();
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            
-        }
-    }
+       
             
 
 }
+    }
